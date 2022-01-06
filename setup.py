7b4022bf0222c33d8a1f0c75936e6da056d61aaf
Ian Fijolek <ian@iamthefij.com>
2022-01-05 20:41:24 -0800

    Update setup.py with supported versions

--- setup.py
+++ setup.py
@@ -24,9 +24,8 @@ setup(
     author_email="",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
     ],
     keywords="",
