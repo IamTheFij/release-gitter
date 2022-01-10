83e76376d0cd0d32a230dfe93323564a28e5b61e
Ian Fijolek <ian@iamthefij.com>
2022-01-10 10:59:55 -0800

    Bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="release-gitter",
-    version="0.3.0",
+    version="0.4.0",
     description="Easily download releases from sites like Github and Gitea",
     long_description=long_description,
     long_description_content_type="text/markdown",
